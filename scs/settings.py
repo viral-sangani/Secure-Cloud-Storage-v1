4ea92f04affccbd65722447e24d897a0feeefdb1
viral <viral.sangani2011@gmai.com>
2019-11-04 10:52:23 +0530

    cors added

--- scs/settings.py
+++ scs/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'account',
     'twofactorauth',
 
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
@@ -51,6 +52,7 @@ ACCOUNT_LOGOUT_ON_GET = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,3 +137,12 @@ STATIC_URL = '/static/'
 
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+# CORS_ORIGIN_WHITELIST = [
+#     '*',
+# ]
+# CORS_ORIGIN_REGEX_WHITELIST = [
+#     '*',
+# ]
