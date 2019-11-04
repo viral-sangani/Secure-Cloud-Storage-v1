c5e31e1b863e9f0eed21544be55191e9e0b78bcb
viral <viral.sangani2011@gmai.com>
2019-11-04 14:32:23 +0530

    git ignore file added

--- scs/settings.py
+++ scs/settings.py
@@ -140,9 +140,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-# CORS_ORIGIN_WHITELIST = [
-#     '*',
-# ]
-# CORS_ORIGIN_REGEX_WHITELIST = [
-#     '*',
-# ]
